7b650c712b02dd40da31aab1f256d9b2cd069327
Er. Sahadev Dahit <sahadevdahit123@gmail.com>
2024-03-10 18:05:32 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello, World! Welcome to the cicd pipeline, testing webhook hoorey'
+        message: 'Hello, World! Welcome to the cicd pipeline, Sucessfull'
     });
 
 });
